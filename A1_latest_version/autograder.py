abc2acbc978d42e923c065070ead0abf2db67cc7
Huakun Shen <huakun.shen@mail.utoronto.ca>
2019-05-30 16:40:58 -0400

    finish Manhattan and L heuristic function with 20 out of 20 solved.
    latest version is correct, do not work on the old version starter code

--- A1_latest_version/autograder.py
+++ A1_latest_version/autograder.py
@@ -4,12 +4,12 @@ from solution import *
 from lunarlockout import LunarLockoutState, Direction, lockout_goal_state #for LunarLockout specific classes and problems
 
 #Select what to test
-test_manhattan = False
+test_manhattan = True
 test_L = True
-test_fval_function = True
+test_fval_function = False
 test_anytime_gbfs = False
-test_alternate = True
-test_anytime_weighted_astar = True
+test_alternate = False
+test_anytime_weighted_astar = False
 
 if test_manhattan:
     ##############################################################
